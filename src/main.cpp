45f79789336d5c0f251c1f1dda52e75b39f503ac
Igor <ivahotin@gmail.com>
2021-11-30 18:15:58 +0300

    Revert "remove all files for review"
    This reverts commit da956a8fe3cd373deaf51aaed7373e4b515a8c2c.

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include "lib.h"
+
+
+int main() {
+
+    ip::print_ip(char(-1));
+    std::cout << std::endl;
+    ip::print_ip(short(0));
+    std::cout << std::endl;
+    ip::print_ip(int(2130706433));
+    std::cout << std::endl;
+    ip::print_ip(long(8875824491850138409));
+    std::cout << std::endl;
+    ip::print_ip(std::string{"127.0.0.1"});
+    std::cout << std::endl;
+    ip::print_ip(std::vector{127, 0, 1, 1});
+    std::cout << std::endl;
+    ip::print_ip(std::list{127, 0, 1, 1});
+    std::cout << std::endl;
+    ip::print_ip(std::make_tuple(255, 255, 255, 255));
+    std::cout << std::endl;
+    ip::print_ip(std::make_tuple(255, 254, 1, 12, 99, 46, 1,23,4, 56, 78));
+    std::cout << std::endl;
+
+    return 0;
+}
